9963230553f1036d13d2e056fa3ad0d10ba902f1
coderx31 <shenal.fernando10@gmail.com>
2021-01-16 10:19:56 +0530

    create the header component and add it to App

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-  const [name, setName] = useState("Shenal");
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key:1},
+    { text: "create app", key:2},
+    { text: "play on the switch",key:3}
+  ]);
  
   return (
     <View style={styles.container}>
-     
+      {/* header */}
+      <Header />
+        <View style={styles.content}>
+          {/* todo form */}
+          <View style={styles.list}>
+              <FlatList 
+              data = { todos }
+              renderItem = { ({ item }) => (
+                <Text> { item.text }</Text>
+              )}
+              />
+          </View>
+        </View>
     </View>
   );
 }
@@ -16,7 +33,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
