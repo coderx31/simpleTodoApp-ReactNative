653b7219267a3e172c1eb353a737df6e35c40fc7
coderx31 <shenal.fernando10@gmail.com>
2021-01-16 09:47:52 +0530

    create the project

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("Shenal");
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
     </View>
   );
 }
